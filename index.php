67200ff316642ac4b8c4417d8b926588a95d2450
michaelx <michimself@googlemail.com>
2011-09-21 00:36:22 +0200

    Version 1.0.2
    * HTML5 valid
    * added url title tags for each entry

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
 	<meta name="keywords" content="Launchbot, Startpage, Script, PHP, Michael Xander, Open-Source">
 	<meta name="description" content="Launchbot enables you to easily open Sets of all your favorite Websites with just one Click. It's like restoring Tabs, but on any Computer and Browser. Additional you can use the build in Google Search Bar for a fresh start. Uhw, and certainly this is not all!">
 	<meta name="author" content="Michael Xander">
-	<meta name="language" content="en">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href="http://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css" >
 	<?php
@@ -32,12 +31,12 @@
 <body OnLoad="document.searchForm.q.focus();">
 
 <div id="wrapper">
-	<div id="favbar"><?php foreach ($favbarsites as $url) {echo "<a href=\"http://".$url."\" target=\"_blank\"><img src=\"http://www.google.com/s2/favicons?domain=".$url."\"></a> ";} ?> &nbsp;</div>
+	<div id="favbar"><?php foreach ($favbarsites as $url) {echo "<a href=\"http://".$url."\" title=\"".$url."\" target=\"_blank\"><img src=\"http://www.google.com/s2/favicons?domain=".$url."\" alt=\"".$url."\"></a> ";} ?> &nbsp;</div>
 	<div id="contentWrapper">
 		<div id="content"> 
 			<h1>&lt; Launchbot &gt;</h1>
 			<div class="search">
-				<p><img src="img/launchbot_eyes.png"></p>
+				<p><img src="img/launchbot_eyes.png" alt="Huh!"></p>
 				<form method="get" action="http://www.google.de/search" id="searchForm" name="searchForm">
 					<fieldset>
 						<input type="text" name="q" maxlength="255" id="searchText" value="" />
@@ -57,11 +56,11 @@
     				echo "window.open('http://".$sets[$i][$u]."'); ";
     			}
     			
-    			echo "\"><img src=\"img/launchbot_sites.png\"><br /><br />".$sets[$i]["setname"]."</a>";
+    			echo "\"><img src=\"img/launchbot_sites.png\" alt=\"Site-Set\"><br /><br />".$sets[$i]["setname"]."</a>";
     			echo "</p>\n <p>";
 
 				for ($u = 0; $u < count($sets[$i])-1; $u++) {
-    				echo "<a href=\"http://".$sets[$i][$u]."\" target=\"_blank\"><img src=\"http://www.google.com/s2/favicons?domain=".$sets[$i][$u]."\"></a>\n ";
+    				echo "<a href=\"http://".$sets[$i][$u]."\" title=\"".$sets[$i][$u]."\" target=\"_blank\"><img src=\"http://www.google.com/s2/favicons?domain=".$sets[$i][$u]."\" alt=\"".$url."\"></a>\n ";
     			}
     			
     			echo "</p>\n </div>\n";
