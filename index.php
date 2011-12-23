fbd2f2f53ff74320f327bc0a26fc506b36e9c97b
michaelx <michimself@googlemail.com>
2011-12-23 19:18:35 +0100

    Version 1.0.4: Switched favicon service [+]
    - Switched from Google's favicon service to the better working
    getFavicon project to fetch favicons.
    - PHP improvements and simplifications

--- index.php
+++ index.php
@@ -22,15 +22,15 @@
 	<meta name="description" content="Launchbot enables you to easily open Sets of all your favorite Websites with just one click. It's like restoring Tabs, but on any Computer and Browser. Additional you can use the built in Google Search Bar for a fresh start. Uhw, and certainly this is not all!">
 	<meta name="author" content="Michael Xander">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<link href="http://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css" >
+	<link href="http://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css">
 	<?php
 		$contentWidth = ((count($sets))*200) + 378;
-		echo "<style type=\"text/css\">#content { width: ".$contentWidth."px; }</style>" 
-	?>	
+		echo '<style type="text/css">#content { width: '.$contentWidth.'px; }</style>'; 
+	?>
 </head>
 <body OnLoad="document.searchForm.q.focus();">
 
-<div id="favbar"><?php foreach ($favbarSites as $url) {echo "<a href=\"http://".$url."\" title=\"".$url."\" target=\"_blank\"><img src=\"http://www.google.com/s2/favicons?domain=".$url."\" alt=\"".$url."\"></a> ";} ?> &nbsp;</div>
+<div id="favbar"><?php foreach ($favbarSites as $url) { echo '<a href="http://'.$url.'" title="'.$url.'" target="_blank"><img src="http://g.etfv.co/http://'.$url.'" alt="'.$url.'" width="16" height="16"></a> '; } ?> &nbsp;</div>
 
 <div id="container">
 	<div id="content">
@@ -46,23 +46,23 @@
 		</div>			
 		<div class="xor">xor open</div>
 					
-		<?php			
+		<?php
 			for ($i = 0; $i < count($sets); $i++) {		
-				echo "<div class=\"set\">";
-				echo "<p class=\"setOpener\"><a href=\"#\" onClick=\"";	
+				echo '<div class="set">';
+				echo '<p class="setOpener"><a href="#" onClick="';	
 
 				for ($u = 0; $u < count($sets[$i])-1; $u++) {
-					echo "window.open('http://".$sets[$i][$u]."'); ";
+					echo 'window.open(\'http://'.$sets[$i][$u].'\'); ';
 				}
 				
-				echo "\"><img src=\"img/launchbot_sites.png\" alt=\"Site-Set\"><br /><br />".$sets[$i]["setname"]."</a>";
-				echo "</p>\n <p>";
+				echo '"><img src="img/launchbot_sites.png" alt="Site-Set"><br /><br />'.$sets[$i]['setname'].'</a>';
+				echo '</p> <p>';
 
 				for ($u = 0; $u < count($sets[$i])-1; $u++) {
-					echo "<a href=\"http://".$sets[$i][$u]."\" title=\"".$sets[$i][$u]."\" target=\"_blank\"><img src=\"http://www.google.com/s2/favicons?domain=".$sets[$i][$u]."\" alt=\"".$url."\"></a>\n ";
+					echo '<a href="http://'.$sets[$i][$u].'" title="'.$sets[$i][$u].'" target="_blank"><img src="http://g.etfv.co/http://'.$sets[$i][$u].'" alt="'.$url.'" width="16" height="16"></a> ';
 				}
 				
-				echo "</p>\n </div>\n";
+				echo '</p> </div>';
 			}
 		?>
 					
